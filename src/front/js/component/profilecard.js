71d435eb2954cd8a83ff0061395b8b7cd42b40b6
Adriana Chiquito <144694058+adrianacp10@users.noreply.github.com>
2024-03-01 18:14:23 +0000

    finder modificado

---
+++ src/front/js/component/profilecard.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+
+
+export const ProfileCard = () => {
+
+    return (
+    <div>hola este es la profile card</div>
+
+    )
+}
