d9cf687d4b0311697b17734852572b8ca2947f7e
Rocio <144937216+Rociosantos18@users.noreply.github.com>
2024-03-06 15:50:22 +0000

    cambios flux

--- src/front/js/pages/MyProfile.js
+++ src/front/js/pages/MyProfile.js
@@ -1,90 +1,41 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import ProfileImg from "../../img/Curly hair-pana.png";
 import "../../styles/MyProfile.css";
 
 export const MyProfile = () => {
     const { store, actions } = useContext(Context);
-    const [editing, setEditing] = useState(false); // Estado para controlar si se está editando o no
-    const [userData, setUserData] = useState({
-        email: "ejemplo@gmail.com",
-        genero: "Femenino",
-        busqueda: "Tengo piso busco roomie",
-        mascota: "Si",
-        presupuesto: "Hasta 300",
-        ubicacion: "Madrid",
-        compiIdeal: "Mucho texto"
-    });
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setUserData({ ...userData, [name]: value });
-    };
+    useEffect(() => {
+        actions.getProfile();
+    }, []);
 
-    const handleEditClick = () => {
-        setEditing(true);
-    };
-
-    const handleCancelClick = () => {
-        setEditing(false);
-        // Si deseas, aquí podrías reiniciar los datos editables a los originales
-    };
-
-    const handleSaveClick = () => {
-        // Aquí puedes agregar la lógica para guardar los datos editados
-        // Puedes llamar a una función de tu contexto para ello, por ejemplo:
-        // actions.actualizarPerfil(userData);
-        setEditing(false);
-    };
+    const { user_name, last_name, email, userProperties } = store;
 
     return (
-        <div className=" container mt-2 p-3 justify-content-center">
+        <div className="container mt-2 p-3 justify-content-center">
             <h3 className="text-center">Mi Perfil</h3>
             <div className="card mb-3">
                 <div className="row">
                     <div className="col-md-4">
-                        <img src={ProfileImg} className="img img-fluid rounded-start" />
+                        <img src={ProfileImg} className="img img-fluid rounded-start" alt="Profile" />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title">Rocio Santos</h5>
-                            {editing ? (
+                            <h5 className="card-title">{user_name} {last_name}</h5>
+                            <p className="card-text m-1">Email: {email}</p>
+                            {userProperties && (
                                 <>
-                                    <input type="text" name="email" value={userData.email} onChange={handleInputChange} />
-                                    <input type="text" name="genero" value={userData.genero} onChange={handleInputChange} />
-                                    <select className="form-select " aria-label="Default select example">
-                                        <option selected>Que buscas?</option>
-                                        <option value="1"> Tengo piso y busco roomie</option>
-                                        <option value="2">Busco roomie con piso</option>
-                                    </select>
-
-                                    <input type="text" name="busqueda" value={userData.busqueda} onChange={handleInputChange} />
-                                    <input type="text" name="mascota" value={userData.mascota} onChange={handleInputChange} />
-                                    <input type="text" name="presupuesto" value={userData.presupuesto} onChange={handleInputChange} />
-                                    <input type="text" name="ubicacion" value={userData.ubicacion} onChange={handleInputChange} />
-                                    <input type="text" name="compiIdeal" value={userData.compiIdeal} onChange={handleInputChange} />
-                                </>
-                            ) : (
-                                <>
-                                    <p className="card-text m-1">Email: {userData.email}</p>
-                                    <p className="card-text m-1">Genero: {userData.genero}</p>
-                                    <p className="card-text m-1">Que es lo que buscas? {userData.busqueda} </p>
-                                    <p className="card-text m-1">Tienes mascota? {userData.mascota} </p>
-                                    <p className="card-text m-1">Cual es tu presupuesto? {userData.presupuesto} </p>
-                                    <p className="card-text m-1">Ubicacion: {userData.ubicacion} </p>
-                                    <p className="card-text m-1">Por que serias el compi ideal? {userData.compiIdeal} </p>
+                                    <p className="card-text m-1">Genero: {userProperties.properties.gender}</p>
+                                    <p className="card-text m-1">Que es lo que buscas? {userProperties.properties.find_roomie}</p>
+                                    <p className="card-text m-1">Tienes mascota? {userProperties.properties.pet}</p>
+                                    <p className="card-text m-1">Cual es tu presupuesto? {userProperties.properties.amount}</p>
+                                    <p className="card-text m-1">Por que serias el compi ideal? {userProperties.properties.text_box}</p>
                                 </>
                             )}
                         </div>
                         <div className="d-flex justify-content-center">
-                            {editing ? (
-                                <>
-                                    <button type="button" className="btn btn-dark me-2" onClick={handleSaveClick}>Guardar</button>
-                                    <button type="button" className="btn btn-dark" onClick={handleCancelClick}>Cancelar</button>
-                                </>
-                            ) : (
-                                <button type="button" className="btn btn-dark" onClick={handleEditClick}>Editar Perfil</button>
-                            )}
+                            <button type="button" className="btn btn-dark">Editar Perfil</button>
                         </div>
                     </div>
                 </div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -7,74 +7,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 			token: null,
 			user_name:null,
 			last_name:null,
-			pet:null,
-			gender:null,
-			budget :null,
-			find_roomie:null,
-			text_box:null,
+			userProperties: null
+			// pet:null,
+			// gender:null,
+			// budget :null,
+			// find_roomie:null,
+			// text_box:null,
 		},
 		actions: {
-			syncTokenFromLocalStorage: () => {
-				const token = localStorage.getItem("token");
-				console.log("application just loaded")
-				if (token && token != "" && token != undefined) setStore({ token: token });
-			},
-
-			SignUp : async (email, password, userName, lastName, setAlertMessage, setEmail, setPassword, setUserName, setLastName) => {
+			getProfile: async () => {
 				try {
-				  const options = {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-					  email: email,
-					  password: password,
-					  user_name: userName,
-					  last_name: lastName
-					})
-				  };
-			
-				  const response = await fetch("https://fluffy-space-bassoon-5gqp59qpxg9wf7gjp-3001.app.github.dev/api/signup", options);
-				  const data = await response.json();
-			
-				  if (data.message === 'Please enter a valid email address') {
-					setAlertMessage(
-					  <div className="alert alert-warning">
-						Por Favor introducir un correo electronico valido
-					  </div>
-					);
-				  } else if (data.message === "Required data is missing") {
-					setAlertMessage(
-					  <div className="alert alert-warning">
-						Faltan Datos
-					  </div>
-					);
-				  } else if (data.message === "The email is already in use") {
-					setAlertMessage(
-					  <div className="alert alert-warning">
-						El Correo Electronico ya existe
-					  </div>
-					);
-				  } else if (data.message === "User created successfully") {
-					setAlertMessage(
-					  <div className="alert alert-success">
-						User creado correctamente
-					  </div>
-					);
-					// Limpiar los campos de entrada después de que se haya creado exitosamente el usuario
-					setEmail("");
-					setPassword("");
-					setUserName("");
-					setLastName("");
-				  }
-				} catch (error) {
-				  throw error;
-				}
-			  },
-
-			getProfile: async () => {}
+					const response = await fetch("https://glowing-cod-x5vgjg7x9gqhp79g-3001.app.github.dev/api/users/profile", {
+						method: "GET",
+						headers: {
+							"Authoritation": "Bearer " + token,
+							"Content-Type": "application/json"
+						}
+					});
+					if (!response.ok){
+						throw new Error(data.message||"Usuario no encontrado")
+					}
+					const userProperties = await response.json();
+					setStore({userProperties: userProperties})
+					
+			} catch (error)	{
+				throw error;
+			}
+		},
 	}
+};
 };
 
 export default getState;
