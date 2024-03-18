fad1e54cbfbf0da29f2410b514ec067c679da5ff
Karen Lyon <144794853+Karelyon@users.noreply.github.com>
2024-03-18 19:31:09 +0000

    changees delete migration

--- src/front/js/pages/CreateProfile.js
+++ src/front/js/pages/CreateProfile.js
@@ -3,7 +3,6 @@ import { Context } from "../store/appContext";
 import "../../styles/createProfile.css";
 import { Link } from "react-router-dom";
 
-
 export const CreateProfile = () => {
     const { store, actions } = useContext(Context);
     const [alertMessage, setAlertMessage] = useState("");
@@ -25,14 +24,13 @@ export const CreateProfile = () => {
             try {
                 await actions.getUserDetails(store);
                 setLoading(false);
-                setUserData(store)
+                setUserData(store);
             } catch (error) {
                 console.error('Error al obtener los detalles del usuario:', error);
             }
         };
 
         fetchUserData();
-
     }, []);
 
     const handleInputChange = (e) => {
@@ -43,11 +41,9 @@ export const CreateProfile = () => {
         });
     };
 
-
     const handleNewImage = async (e) => {
         const image = e.target.files[0];
         setImage(image);
-        console.log("soy imagen")
         const formDataImage = new FormData();
         formDataImage.append("file", image);
         formDataImage.append("upload_preset", "injqzpue");
@@ -90,107 +86,100 @@ export const CreateProfile = () => {
             console.error("Error al enviar datos:", error);
             setAlertMessage("Error al crear el perfil");
         }
-    }
+    };
 
     if (loading) {
         return (
             <div className="spinner-border text-primary" role="status">
                 <span className="visually-hidden">Cargando...</span>
             </div>
-        )
+        );
     }
 
     return (
-        <div className="container mt-2 p-3 justify-content-center">
-
+        <div className="container mt-2 p-3">
             <div className="createProfile">
-                <div className="container createProfilePage mt-2 p-3 justify-content-center">
-                    <form className="d-flex">
-                        <div className="form-group container imageGroup col-4">
-                            <div className="profileImg">
-                                {image ? (
-                                    <img src={URL.createObjectURL(image)} alt="Uploaded" className="uploaded-img " />
-                                ) : (
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsg0F0hqjo2pVSEgusU_JvJ4WOxd-U1QWMnw&usqp=CAU" alt="Placeholder" className="placeholder-img " />
-                                )}
+                <div className="createProfilePage mt-2 p-3">
+                    <form>
+                        <div className="row">
+                            <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                                <div className="profileImg">
+                                    {image ? (
+                                        <img src={URL.createObjectURL(image)} alt="Uploaded" className="uploaded-img img-fluid" />
+                                    ) : (
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsg0F0hqjo2pVSEgusU_JvJ4WOxd-U1QWMnw&usqp=CAU" alt="Placeholder" className="placeholder-img img-fluid" />
+                                    )}
+                                </div>
+                                <input
+                                    id="fileInput"
+                                    name="profile_img"
+                                    type="file"
+                                    hidden // Oculta el input
+                                    onChange={handleNewImage}
+                                />
+                                <label htmlFor="fileInput" className="labelImg btn btn-primary mt-3">
+                                    Escoge una imagen
+                                </label>
                             </div>
-                            <input
-                                id="fileInput"
-                                name="profile_img"
-                                type="file"
-                                hidden // Oculta el input
-                                onChange={handleNewImage}
-
-                            />
-                            <label htmlFor="fileInput" className="labelImg btn">
-                                Escoge una imagen
-                            </label>
-
+                            <div className="col-lg-8 col-md-6 col-sm-12">
+                                <div className="nameCreateProfile mb-3">
+                                    <h3>{userData.user_name} {userData.last_name}</h3>
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Que buscas?</label>
+                                        <select className="form-select" name="find_roomie" value={formData.find_roomie} onChange={handleInputChange}>
+                                            <option value="">Que buscas?</option>
+                                            <option value="Apartment">Tengo piso y busco roomie</option>
+                                            <option value="NoApartment">Busco roomie con piso</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Cual es tu presupuesto?</label>
+                                        <input type="text" className="form-control" id="budget" name="budget" value={formData.budget} onChange={handleInputChange} />
+                                    </div>
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Tienes mascota</label>
+                                        <select className="form-select" name="pet" value={formData.pet} onChange={handleInputChange}>
+                                            <option value="">Tienes mascota?</option>
+                                            <option value="Yes">Si</option>
+                                            <option value="No">No</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Genero</label>
+                                        <select className="form-select" name="gender" value={formData.gender} onChange={handleInputChange}>
+                                            <option value="">Selecciona te genero</option>
+                                            <option value="Female">Mujer</option>
+                                            <option value="Male">Hombre</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label fw-bold">Por que serias el compi ideal?</label>
+                                    <textarea
+                                        className="form-control"
+                                        id="exampleFormControlTextarea1"
+                                        rows="3"
+                                        value={formData.text_box}
+                                        onChange={handleInputChange}
+                                        name="text_box"
+                                    ></textarea>
+                                </div>
+                            </div>
                         </div>
-
-                        <div className="container inputsCreateProfile">
-                            <div className="container nameCreateProfile">
-                                <h3>{userData.user_name} {userData.last_name}</h3>
-                            </div>
-                            <div className="container d-flex">
-                                <div className="m-1 inputOption">
-                                    <label className="form-label mt-2 fw-bold">Que buscas?</label>
-                                    <select className="form-select " name="find_roomie" value={formData.find_roomie} onChange={handleInputChange}>
-                                        <option value="">Que buscas?</option>
-                                        <option value="Apartment">Tengo piso y busco roomie</option>
-                                        <option value="NoApartment">Busco roomie con piso</option>
-                                    </select>
-                                </div>
-                                <div className="m-1 inputOption">
-                                    <label className="form-label mt-2 fw-bold">Cual es tu presupuesto?</label>
-                                    <input type="text" className="form-control" id="budget" name="budget" value={formData.budget} onChange={handleInputChange} />
-                                </div>
-                            </div>
-                            <div className="container d-flex">
-                                <div className="m-1 inputOption ">
-                                    <label className="form-label mt-2 fw-bold">Tienes mascota</label>
-                                    <select className="form-select" name="pet" value={formData.pet} onChange={handleInputChange}>
-                                        <option value="">Tienes mascota?</option>
-                                        <option value="Yes">Si</option>
-                                        <option value="No">No</option>
-                                    </select>
-                                </div>
-                                <div className="m-1 inputOption">
-                                    <label className="form-label mt-2 fw-bold">Genero</label>
-                                    <select className="form-select " name="gender" value={formData.gender} onChange={handleInputChange}>
-                                        <option value="">Selecciona te genero</option>
-                                        <option value="Female">Mujer</option>
-                                        <option value="Male">Hombre</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="m-1 container ">
-                                <label className="form-label mt-2 fw-bold" name="text_box">Por que serias el compi ideal?</label>
-                                <textarea
-                                    className="form-control"
-                                    id="exampleFormControlTextarea1"
-                                    rows="3"
-                                    value={formData.text_box}
-                                    onChange={handleInputChange}
-                                    name="text_box"
-                                ></textarea>
-                            </div>
+                        {alertMessage && (<div className="alert alert-danger">{alertMessage}</div>)}
+                        <div className="buttonsCP mt-3">
+                            <button type="button " className="btn btn-dark me-2" onClick={handleSubmit} disabled={!imageUploaded}>Crear Perfil</button>
+                            <Link to={"/password"}>
+                                <button type="button" className="btn btn-dark">Cambiar Contrasena</button>
+                            </Link>
                         </div>
-
-
-                        {alertMessage && (<div className="container alert alert-danger mt-3">{alertMessage}</div>)}
                     </form>
-                    <div className="buttonsCP mt-5">
-                        <Link to={"/profile"}>
-                            <button type="button" className="btn btn-dark me-2" onClick={(e) => handleSubmit(e)} disabled={!imageUploaded}>Crear Perfil</button>
-                        </Link>
-                        <Link to={"/password"}>
-                            <button type="button" className="btn btn-dark">Cambiar Contrasena</button>
-                        </Link>
-                    </div>
                 </div>
             </div>
         </div>
     );
-
-}
+};
--- src/front/js/pages/EditProfile.js
+++ src/front/js/pages/EditProfile.js
@@ -6,31 +6,21 @@ import { Link } from "react-router-dom";
 export const EditProfile = () => {
     const { store, actions } = useContext(Context);
     const [alertMessage, setAlertMessage] = useState("");
-
     const [formData, setFormData] = useState({
-        pet: store.pet || "", // Cargar las opciones existentes del store
+        pet: store.pet || "",
         gender: store.gender || "",
         budget: store.budget || "",
         find_roomie: store.find_roomie || "",
         text_box: store.text_box || "",
-        profile_img: store.profile_img || null // Inicialmente, establece la imagen como nula
+        profile_img: store.profile_img || null
     });
-
     const [image, setImage] = useState(null);
 
-
     useEffect(() => {
-        // Cargar las opciones existentes del store en el estado formData
         setFormData({
             ...formData,
             last_name: store.last_name || "",
-            user_name: store.user_name || "",
-            pet: store.pet || "",
-            gender: store.gender || "",
-            budget: store.budget || "",
-            find_roomie: store.find_roomie || "",
-            text_box: store.text_box || "",
-            profile_img: store.profile_img || null
+            user_name: store.user_name || ""
         });
     }, []);
 
@@ -44,14 +34,14 @@ export const EditProfile = () => {
 
     const handleNewImage = async (e) => {
         const image = e.target.files[0];
-        setImage(image); // Actualiza la imagen en el estado
+        setImage(image);
         const formDataImage = new FormData();
         formDataImage.append("file", image);
-        formDataImage.append("upload_preset", "injqzpue"); // Reemplaza "tu-upload-preset" con tu upload preset
+        formDataImage.append("upload_preset", "injqzpue");
 
         try {
             const response = await fetch(
-                'https://api.cloudinary.com/v1_1/dru67quag/image/upload', // Reemplaza "tu-cloud-name" con el nombre de tu nube en Cloudinary
+                'https://api.cloudinary.com/v1_1/dru67quag/image/upload',
                 {
                     method: "POST",
                     body: formDataImage,
@@ -60,7 +50,7 @@ export const EditProfile = () => {
             const data = await response.json();
             setFormData({
                 ...formData,
-                profile_img: data.secure_url // Actualiza el estado con la URL de la imagen cargada
+                profile_img: data.secure_url
             });
         } catch (error) {
             console.error("Error al cargar la imagen:", error);
@@ -70,103 +60,89 @@ export const EditProfile = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await actions.updateProfileInfo(formData); // Enviar directamente el estado formData
-            setAlertMessage(""); // Borra cualquier mensaje de alerta previo
-
+            await actions.updateProfileInfo(formData);
+            setAlertMessage("");
         } catch (error) {
             console.error("Error al enviar datos:", error);
-            setAlertMessage("Error al crear el perfil"); // Establece el mensaje de error
+            setAlertMessage("Error al crear el perfil");
         }
     };
 
     return (
-        <div className="container mt-2 p-3 justify-content-center">
-
+        <div className="container mt-2 p-3">
             <div className="createProfile">
-                <div className="container createProfilePage mt-2 p-3 justify-content-center">
-                    <form className="d-flex">
-                        <div className="form-group container imageGroup col-4">
-                            <div className="profileImg">
-                                {image ? (
-                                    <img src={URL.createObjectURL(image)} alt="Uploaded" className="uploaded-img " />
-                                ) : (
-                                    <img src={formData.profile_img} alt="Placeholder" className="placeholder-img " />
-                                )}
-                            </div>
-                            <input
-                                id="fileInput"
-                                name="profile_img"
-                                type="file"
-                                hidden // Oculta el input
-                                onChange={handleNewImage}
-
-                            />
-                            <label htmlFor="fileInput" className="labelImg btn">
-                                Escoge una imagen
-                            </label>
-
-                        </div>
-
-
-
-                        <div className="container inputsCreateProfile">
-                            <div className="container nameCreateProfile">
-                                <h3>{formData.user_name} {formData.last_name}</h3>
-                            </div>
-                            <div className="container d-flex">
-                                <div className="m-1 inputOption">
-                                    <label className="form-label mt-2 fw-bold">Que buscas?</label>
-                                    <select className="form-select " name="find_roomie" value={formData.find_roomie} onChange={handleInputChange}>
-                                        <option value="">Que buscas?</option>
-                                        <option value="Apartment">Tengo piso y busco roomie</option>
-                                        <option value="NoApartment">Busco roomie con piso</option>
-                                    </select>
+                <div className="createProfilePage mt-2 p-3">
+                    <form>
+                        <div className="row">
+                            <div className="form-group container imageGroup col-lg-4 col-md-6 col-sm-12">
+                                <div className="profileImg">
+                                    {image ? (
+                                        <img src={URL.createObjectURL(image)} alt="Uploaded" className="uploaded-img" />
+                                    ) : (
+                                        <img src={formData.profile_img || "https://via.placeholder.com/150"} alt="Placeholder" className="placeholder-img" />
+                                    )}
                                 </div>
-                                <div className="m-1 inputOption">
-                                    <label className="form-label mt-2 fw-bold">Cual es tu presupuesto?</label>
-                                    <input type="text" className="form-control" id="budget" name="budget" value={formData.budget} onChange={handleInputChange} />
-                                </div>
+                                <input
+                                    id="fileInput"
+                                    name="profile_img"
+                                    type="file"
+                                    hidden
+                                    onChange={handleNewImage}
+                                />
+                                <label htmlFor="fileInput" className="labelImg btn">
+                                    Escoge una imagen
+                                </label>
                             </div>
-                            <div className="container d-flex ">
-                                <div className="m-1 inputOption ">
-                                    <label className="form-label mt-2 fw-bold">Tienes mascota</label>
-                                    <select className="form-select" name="pet" value={formData.pet} onChange={handleInputChange}>
-                                        <option value="">Tienes mascota?</option>
-                                        <option value="Yes">Si</option>
-                                        <option value="No">No</option>
-                                    </select>
+                            <div className="container inputsCreateProfile col-lg-8 col-md-6 col-sm-12">
+                                <div className="nameCreateProfile mb-3">
+                                    <h3>{formData.user_name} {formData.last_name}</h3>
                                 </div>
-                                <div className="m-1 inputOption ">
-                                    <label className="form-label mt-2 fw-bold">Genero</label>
-                                    <select className="form-select " name="gender" value={formData.gender} onChange={handleInputChange}>
-                                        <option value="">Selecciona te genero</option>
-                                        <option value="Female">Mujer</option>
-                                        <option value="Male">Hombre</option>
-                                    </select>
+                                <div className="row mb-3">
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Que buscas?</label>
+                                        <select className="form-select" name="find_roomie" value={formData.find_roomie} onChange={handleInputChange}>
+                                            <option value="">Que buscas?</option>
+                                            <option value="Apartment">Tengo piso y busco roomie</option>
+                                            <option value="NoApartment">Busco roomie con piso</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Cual es tu presupuesto?</label>
+                                        <input type="text" className="form-control" id="budget" name="budget" value={formData.budget} onChange={handleInputChange} />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="m-1 container">
-                                <label className="form-label mt-2 fw-bold" name="text_box">Por que serias el compi ideal?</label>
-                                <textarea
-                                    className="form-control"
-                                    id="exampleFormControlTextarea1"
-                                    rows="3"
-                                    value={formData.text_box}
-                                    onChange={handleInputChange}
-                                    name="text_box"
-                                ></textarea>
+                                <div className="row mb-3">
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Tienes mascota</label>
+                                        <select className="form-select" name="pet" value={formData.pet} onChange={handleInputChange}>
+                                            <option value="">Tienes mascota?</option>
+                                            <option value="Yes">Si</option>
+                                            <option value="No">No</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <label className="form-label fw-bold">Genero</label>
+                                        <select className="form-select" name="gender" value={formData.gender} onChange={handleInputChange}>
+                                            <option value="">Selecciona te genero</option>
+                                            <option value="Female">Mujer</option>
+                                            <option value="Male">Hombre</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label fw-bold">Por que serias el compi ideal?</label>
+                                    <textarea className="form-control" rows="3" value={formData.text_box} onChange={handleInputChange} name="text_box"></textarea>
+                                </div>
                             </div>
                         </div>
-                        {alertMessage && (<div className="container alert alert-danger mt-3">{alertMessage}</div>)}
+                        {alertMessage && (<div className="alert alert-danger">{alertMessage}</div>)}
+                        <div className="buttonsCP mt-3">
+                            <button type="button" className="btn btn-dark me-2" onClick={handleSubmit}>Guardar</button>
+                            <Link to={"/password"}>
+                                <button type="button" className="btn btn-dark">Cambiar Contrasena</button>
+                            </Link>
+                        </div>
                     </form>
-                    <div className="buttonsCP mt-5">
-
-                        <button type="button" className="btn btn-dark me-2" onClick={handleSubmit}>Guardar</button>
-
-                        <Link to={"/password"}>
-                            <button type="button" className="btn btn-dark">Cambiar Contrasena</button>
-                        </Link>
-                    </div>
                 </div>
             </div>
         </div>
