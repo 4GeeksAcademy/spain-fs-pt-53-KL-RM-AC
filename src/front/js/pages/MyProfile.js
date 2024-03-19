0053121bd710f03fac78e3f0680369edf9b1e4f1
Adriana Chiquito <144694058+adrianacp10@users.noreply.github.com>
2024-03-19 18:38:36 +0000

    falta ruta de favs

--- src/front/js/pages/MyProfile.js
+++ src/front/js/pages/MyProfile.js
@@ -17,12 +17,11 @@ const LITERALS = {
 export const MyProfile = () => {
     const { store, actions } = useContext(Context);
     const userData = {
-
         user_name: store.user_name,
         last_name: store.last_name,
         pet: store.pet,
-        email: store.email,
         gender: store.gender,
+        email: store.email,
         budget: store.budget,
         find_roomie: store.find_roomie,
         text_box: store.text_box,
@@ -30,15 +29,12 @@ export const MyProfile = () => {
     };
     const [loading, setLoading] = useState(true);
 
-
     useEffect(() => {
         const fetchProfile = async () => {
             try {
                 setLoading(true);
                 await actions.getProfile();
-
-                setLoading(false); // Cuando getProfile() ha terminado, establece loading como false
-
+                setLoading(false);
             } catch (error) {
                 console.error("Error al obtener perfil:", error);
                 setLoading(false);
@@ -58,7 +54,7 @@ export const MyProfile = () => {
             userData.gender &&
             userData.budget &&
             userData.find_roomie &&
-            userData.profile_img && 
+            userData.profile_img &&
             userData.text_box &&
             userData.pet
         );
@@ -74,40 +70,13 @@ export const MyProfile = () => {
         }
 
         return (
-
-            <div className="nuevo">
+            <div className="nuevo"> 
                 {hasRequiredFields() ? (
                     <div className="container myprofile mt-4 p-3 justify-content-center"> 
-                        <div className="postal">
+                        <div className="postal-myprofile">
                             <div className="row">
                                 <div className="col-md-6 d-flex justify-content-center align-items-center pb-3">
                                     <img src={userData.profile_img} className="profile-image img-fluid rounded-circle" alt="Profile" style={{ width: '400px', height: '400px', objectFit: 'cover' }} />
-
-            <div className="spinner-border text-success" role="status">
-                <span className="sr-only">Cargando...</span>
-            </div>
-        );}
-    
-    return (
-        <div >
-            {hasRequiredFields() ? (
-                <>
-                    <h3 className="text-center">Mi Perfil</h3>
-                    <div className="card mb-3">
-                        <div className="row">
-                            <div className="col-md-4">
-                                <img src={userData.profile_img} className="img img-fluid rounded-start" alt="Profile" />
-                            </div>
-                            <div className="col-md-8">
-                                <div className="card-body">
-                                    <h5 className="card-title">{userData.user_name} {userData.last_name}</h5>
-                                    <p className="card-text m-1">Email: {userData.email}</p>
-                                    <p className="card-text m-1">Genero: {userData.gender}</p>
-                                    <p className="card-text m-1">Que buscas? {userData.find_roomie}</p>
-                                    <p className="card-text m-1">Tienes mascota? {userData.pet}</p>
-                                    <p className="card-text m-1">Cual es tu presupuesto? {userData.budget}</p>
-                                    <p className="card-text m-1">Por que serias el compi ideal? {userData.text_box}</p>
-
                                 </div>
                                 <div className="col-md-6 user-info">
                                     <h2 className="text-center">Mi Perfil</h2>
